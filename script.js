0960be4a47593cde4be7ed574430d647b8574215
chaudhary64 <chiragchoudhary64@gmail.com>
2024-03-15 16:31:19 +0530

    Implemented the logic of the loader screen

---
+++ script.js
@@ -0,0 +1,13 @@
+function coundown() {
+  let counter = 0;
+  let counterElem = document.getElementById("counter");
+
+  let timer = setInterval(() => {
+    if (counter === 100) {
+      clearInterval(timer);
+    }
+    counterElem.textContent = counter;
+    counter++;
+  }, 20);
+}
+coundown();
