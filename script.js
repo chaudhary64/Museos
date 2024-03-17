0548621fa80192a2fdd51f12627b2ef9b23084c1
chaudhary64 <chiragchoudhary64@gmail.com>
2024-03-17 16:23:09 +0530

    Animation of image with scrolling

--- script.js
+++ script.js
@@ -31,3 +31,64 @@ function LoaderRemover() {
   });
 }
 LoaderRemover();
+
+function imageGatherer(){
+  let ip1 = document.querySelector("#ip1");
+let ip2 = document.querySelector("#ip2");
+let ip3 = document.querySelector("#ip3");
+let ip4 = document.querySelector("#ip4");
+
+
+gsap.to("#ip1", {
+  objectPosition: "top",
+  scale : 1,
+  stagger: 0.5,
+  scrollTrigger: {
+    trigger: "#ip1",
+    scroll : "body",
+    scrub : 5,
+    start: "400px 30%",
+    end: "500px 20%",
+  }
+});
+
+gsap.to("#ip2", {
+  objectPosition: "0% -20vh",
+  scale : 1,
+  stagger: 0.5,
+  scrollTrigger: {
+    trigger: "#ip2",
+    scroll : "body",
+    scrub : 5,
+    start: "400px 30%",
+    end: "430px 20%",
+  }
+});
+
+gsap.to("#ip3", {
+  objectPosition: "0% -80vh",
+  scale : 1,
+  stagger: 0.5,
+  scrollTrigger: {
+    trigger: "#ip3",
+    scroll : "body",
+    scrub : 5,
+    start: "400px 30%",
+    end: "430px 20%",
+  }
+});
+
+gsap.to("#ip4", {
+  objectPosition: "0% -100vh",
+  scale : 1,
+  stagger: 0.5,
+  scrollTrigger: {
+    trigger: "#ip4",
+    scroll : "body",
+    scrub : 5,
+    start: "400px 30%",
+    end: "430px 20%",
+  }
+});
+}
+imageGatherer();
