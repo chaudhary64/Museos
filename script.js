02491377265998b4fb809983dfeabb9117e030c2
chaudhary64 <chiragchoudhary64@gmail.com>
2024-04-15 20:12:31 +0530

    Changed the ImageGatherer function to get the desired result

--- script.js
+++ script.js
@@ -41,54 +41,32 @@ function LoaderRemover() {
 LoaderRemover();
 
 function ImageGatherer() {
-  let img1 = document.querySelector(".img1");
-  let img2 = document.querySelector(".img2");
-  let img3 = document.querySelector(".img3");
-  let img4 = document.querySelector(".img4");
-
-  gsap.to(".img1", {
-    scale: 1,
-    objectPosition: "0 0",
+  let tl = gsap.timeline({
     scrollTrigger: {
-      trigger: ".imgcont1",
-      start: "10% 55%",
-      end: "bottom 20%",
-      scrub: true,
+      trigger: "#ImageContainer",
+      start: "top 0%",
+      end: "bottom -200%",
+      scrub: 1,
+      pin: true,
     },
   });
 
-  gsap.to(".img2", {
-    scale: 1,
-    objectPosition: "0 -20vh",
-    scrollTrigger: {
-      trigger: ".imgcont2",
-      start: "top 75%",
-      end: "40% 70%",
-      scrub: true,
+  tl.to(
+    "#upperAndLowerImg",
+    {
+      scale: 1,
+      transform: "translate(0%,0%)",
     },
-  });
-
-  gsap.to(".img3", {
-    scale: 1,
-    objectPosition: "0 -100vh",
-    scrollTrigger: {
-      trigger: ".imgcont3",
-      start: "top 95%",
-      end: "30% 80%",
-      scrub: true,
+    "GatherTheImagesTogether"
+  );
+  tl.to(
+    "#centerImg",
+    {
+      scale: 1,
+      transform: "translate(0%,-50%)",
     },
-  });
-
-  gsap.to(".img4", {
-    scale: 1,
-    objectPosition: "0 -140vh",
-    scrollTrigger: {
-      trigger: ".imgcont4",
-      start: "top 100%",
-      end: "30% 90%",
-      scrub: true,
-    },
-  });
+    "GatherTheImagesTogether"
+  );
 }
 ImageGatherer();
 
@@ -135,7 +113,7 @@ function CoverTheStrip() {
   let cover2 = document.querySelector(".cover2");
   let cover3 = document.querySelector(".cover3");
 
-  function coverIt(elem , cover) {
+  function coverIt(elem, cover) {
     // Mouseenetr Event
     elem.addEventListener("mouseenter", () => {
       gsap.to(cover, {
@@ -144,8 +122,8 @@ function CoverTheStrip() {
         ease: "power1.inOut",
       });
       elem.style.color = "black";
-      elem.style.transition = "transform 0.3s"
-      elem.style.transform = "TranslateX(2%)"
+      elem.style.transition = "transform 0.3s";
+      elem.style.transform = "TranslateX(2%)";
     });
 
     // Mouseleave Event
@@ -156,11 +134,11 @@ function CoverTheStrip() {
         ease: "power1.inOut",
       });
       elem.style.color = "white";
-      elem.style.transform = "TranslateX(0%)"
+      elem.style.transform = "TranslateX(0%)";
     });
   }
-  coverIt(strip1,cover1);
-  coverIt(strip2,cover2);
-  coverIt(strip3,cover3);
+  coverIt(strip1, cover1);
+  coverIt(strip2, cover2);
+  coverIt(strip3, cover3);
 }
 CoverTheStrip();
